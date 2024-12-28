0d6091e92da92db3d02445cef4dd7c021cf1659e
Szymon Malinowski <smalinow@c2r8s3.42warsaw.pl>
2024-12-28 18:28:56 +0100

    hex update

--- print_hex.c
+++ print_hex.c
@@ -12,9 +12,48 @@
 #include "libft.h"
 #include "ft_printf.h"
 
+static void reverse_string(char *str)
+{
+	int		i;
+	int		len;
+	char 	temp;
+
+	i = 0;
+	len = ft_strlen(str);
+	while (i < len / 2)
+	{
+		temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+		i++;
+	}
+}
+
 int print_hex(int nbr)
 {
-    char base[16];
-    
-    *base = "012356789abcdef";
+	char	base[16] = "0123456789abcdef";
+	char	result[64];
+	int		i;
+	if (nbr == -2147483648)
+	{
+		ft_putstr_fd("80000000", 1);
+		return (8);
+	}
+	if (nbr == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	i = 0;
+	if (nbr < 0)
+		nbr = -nbr;
+	while (nbr > 0)
+	{
+		result[i++] = base[nbr % 16];
+		nbr /= 16;
+	}
+	result[i] = '\0';
+	reverse_string(result);
+	ft_putstr_fd(result, 1);
+	return (ft_strlen(result));
 }
